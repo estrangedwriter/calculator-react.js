0e0fa835d948113aadac7d4fb595ab4dc3f8f520
estrangedwriter <jeromezhang1996@gmail.com>
2020-09-14 12:38:35 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Button from './Button.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      current: '',
+      previous:[]
+    }
+  }
+
+
+  reset = () => {
+    this.setState({result: '0'});
+
+  }
+
+  render() {
+    const buttons = [
+      {symbol: 'C', cols: 1, action: this.reset}
+    ];
+
+    return(
+      <div className = "App">
+        <input className="Result" type="text" value={this.state.current}/>
+
+
+
+      </div>
+    );
+
+  }
 }
 
 export default App;
---
+++ src/Button.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import App from './App';
+
+
+class Button extends Component {
+    render() {
+        return (
+            <div>
+                <button class = "calc-button">1</button>
+            </div>
+        )
+    }
+}
+
+export default Button;
